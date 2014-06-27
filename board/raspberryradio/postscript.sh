c51e08c4761f85cd079290785c90ec533ac734f8
Christian Erhardt <christian.erhardt@mojo2k.de>
2014-06-27 19:54:27 +0200

    network and mpd working

--- board/raspberryradio/postscript.sh
+++ board/raspberryradio/postscript.sh
@@ -8,8 +8,12 @@ cp ${TARGET}/../build/linux-rpi-3.12.y/System.map ${TARGET}/System.map
 # move kernel
 mv ${TARGET}/../images/zImage ${TARGET}/../images/rpi-firmware/
 
-# move network
+# move network to start later - hardware is not fast enough
 mv ${TARGET}/etc/init.d/S40network ${TARGET}/etc/init.d/S90network
+
+# move resolv.conf
+rm ${TARGET}/etc/resolv.conf
+mv ${TARGET}/etc/resolv.conf_bkp ${TARGET}/etc/resolv.conf
 
 exit 0
 
