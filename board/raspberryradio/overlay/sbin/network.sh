616477da5df344f0c9adca680151e462c0ded56a
Christian Erhardt <christian.erhardt@mojo2k.de>
2014-07-21 21:33:16 +0200

    Changing configuration

--- board/raspberryradio/overlay/sbin/network.sh
+++ board/raspberryradio/overlay/sbin/network.sh
@@ -1,25 +1,56 @@
-#!/bin/ash
+#!/bin/sh
+#
+# Start wifi connection
+#
 
 . /sdcard/conf/network.conf
 
-startWifi() {
+# starts the wifi connection and waits for the link to come up
+start() {
   ifdown -f $iface
   killall -q wpa_supplicant
   wpa_supplicant -Dwext -i $iface -c /sdcard/conf/wifi.conf -B
   ifconfig $iface $ip netmask $netmask up
   route add default gw $gateway
-  sleep 30
-  
+  unreachable=1;
+  count=0;
+  echo "Waiting for connection to come up "
+  while [ $unreachable -ne "0" || $count -le $timeout ];
+  do
+     ping -q -c 1 $gateway &> /dev/null; 
+     unreachable=$?; 
+     sleep 1;
+     count = $count + 1;
+     echo -n ".";
+  done
+  echo "Connection established after $count seconds"
 }
 
-startWifi
+# monitor the wifi connection for possible loss
+monitor () {
+   # check every 30 seconds for connection loss
+   while [ 1 ]; do
+      ping -c 1 $gateway & wait $!
+      if [ $? != 0 ]; then
+        startWifi
+      else
+        sleep 30
+      fi
+   done
+}
 
-while [ 1 ]; do
-   ping -c 1 $gateway & wait $!
-   if [ $? != 0 ]; then
-     startWifi
-     sleep 1
-   else sleep 10
-   fi
-done
 
+case "$1" in
+  start)
+          start
+          ;;
+  monitor)
+          monitor
+          ;;
+  *)
+          echo "Usage: $0 {start|monitor}"
+          exit 1
+          ;;
+esac
+
+exit $?
